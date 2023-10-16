f0f4cfe7d4ca728fc62ea0763dc0bc34c0b3e27e
Jonathan Correa Paiva <jona.correapc@gmail.com>
2023-10-16 16:34:09 -0300

    update: change footer position

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,20 @@ import RatingPanel from './rating'
 function App() {
   return (
     <Grid
-      templateRows='1fr auto'
+      templateRows='1fr'
       templateColumns='auto'
       gap={4}
       bgColor={'background'} 
       minH="100vh" 
       minW={'375px'}
       w={'100vw'} 
-      justifyContent={'center'}>
+      justifyContent={'center'}
+      alignItems={'center'}
+      >
       <Center as='main'>
         <RatingPanel />
       </Center>
-      <Flex py={'1rem'} as={'footer'} className="attribution" alignItems={'center'} justifySelf={'center'} direction={{base: 'column', md: 'row'}} gap={2}>
+      <Flex py={'1rem'} as={'footer'} className="attribution" alignItems={'center'} justifySelf={'center'} direction={{base: 'column', md: 'row'}} gap={2} position={'absolute'} bottom={0}>
         <Box>Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.</Box>
         <Box>Coded by <a href="https://github.com/Jonakss">Jonathan Correa | Jonakss</a>.</Box>
       </Flex>
